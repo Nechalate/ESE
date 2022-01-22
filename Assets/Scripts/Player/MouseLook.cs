3f658bb0295d783b8f7760fdc4c8336d9f736897
Nechalate <penpolpen@gmail.com>
2022-01-22 09:21:25 +0300

    resave project

---
+++ Assets/Scripts/Player/MouseLook.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] Transform playerBody;
+    [SerializeField] float mouseSensivity = 150f; 
+    float xRotation = 0f;
+    void Start()
+    {
+        CursorLock();
+    }
+
+    void Update()
+    {
+        MouseRotate();
+    }
+
+    void MouseRotate() {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    void CursorLock() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] CharacterController controller;
+    [SerializeField] float moveSpeed = 10f;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        CharacterMove();
+    }
+
+    void CharacterMove() {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+    }
+}
