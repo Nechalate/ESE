2ac03e7f9b0b0a90c6906a8f6993db50fde0dd37
Nechalate <penpolpen@gmail.com>
2022-01-29 00:13:35 +0300

    add keycard model and texture, add sound on taking keycard

---
+++ Assets/Scripts/Items/PickupKey.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupKey : MonoBehaviour
+{
+    AudioSource audioSource;
+    [SerializeField] AudioClip pickupItemSound;
+    
+    void Start() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            if (audioSource.isPlaying != true) {
+                audioSource.PlayOneShot(pickupItemSound);
+            }
+            Destroy(gameObject, 0.5f);
+        }
+    }
+}
