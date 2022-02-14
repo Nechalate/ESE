8e391bb30d8450e859a61a4dd682637627587c1c
Nechalate <penpolpen@gmail.com>
2022-02-14 22:00:23 +0300

    fixed map bugs, add player physics

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,7 +3,14 @@
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] CharacterController controller;
+    public Transform groundCheck;
+    public LayerMask groundMask;
     [SerializeField] float moveSpeed = 10f;
+
+    float gravity = -9.8f;
+    Vector3 velocity;
+    bool isGround;
+    float groundDistance = 0.4f;
     void Start()
     {
         
@@ -15,10 +22,33 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void CharacterMove() {
+
+        GroundPhysics();
+        VectorInput();
+        Fall();
+    }
+
+    void GroundPhysics() {
+        isGround = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGround && velocity.y < 0) {
+            velocity.y = -2f;
+        }
+    }
+
+    void VectorInput() {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * moveSpeed * Time.deltaTime);
+    }
+
+    void Fall() {
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void Jump() {
+        
     }
 }
