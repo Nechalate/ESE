756c7420844b7431a48763d60426f2ce50d268c9
Nechalate <penpolpen@gmail.com>
2022-02-21 00:51:31 +0300

    added door animation, added card interaction, added new textures. plans: create aperture for door, fix check bool take card

---
+++ Assets/Scripts/Doors/DoorSystem.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class DoorSystem : MonoBehaviour
+{
+    [SerializeField] AudioClip doorSound;
+    [SerializeField] Animator animator;
+    AudioSource audioSource;
+    
+    
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+    }
+    
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            if (other.GetComponent<KeyControl>().KeyHas == true) {
+                OpenDoor();
+            }
+        }
+    }
+
+    void OpenDoor() {
+        animator.Play("DoorOpenLevel1");
+        audioSource.PlayOneShot(doorSound);
+    }
+}
---
+++ Assets/Scripts/Player/KeyControl.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class KeyControl : MonoBehaviour
+{
+    public bool KeyHas;
+}
