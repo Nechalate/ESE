8eb3dee84f05d3a0e997bd76c9acd2e7e0e99535
Nechalate <penpolpen@gmail.com>
2022-02-22 22:35:24 +0300

    added fan prefab with particle system(dust) and incident light + lamp

---
+++ Assets/Scripts/Complex/FanScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FanScript : MonoBehaviour
+{
+    [SerializeField] Animator _anim;
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+        _anim.Play("Active");
+    }
+
+}
