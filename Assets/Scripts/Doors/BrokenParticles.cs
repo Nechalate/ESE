49a5df16498fbc782c7540cc279fd7b0312324ec
Nechalate <p1-18@STUD.LOCAL>
2022-03-02 16:01:52 +0300

    change project version, fixed light bugs, added broken door particles

---
+++ Assets/Scripts/Doors/BrokenParticles.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class BrokenParticles : MonoBehaviour
+{
+    [SerializeField] ParticleSystem brokenParticle;
+
+    void Update()
+    {
+        Invoke("ParticleBoom", 3);
+    }
+
+    void ParticleBoom() {
+        if (!brokenParticle.isPlaying) brokenParticle.Play();
+    }
+}
