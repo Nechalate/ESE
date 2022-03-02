0c67c02676d361788a3ba7714285052385867666
Nechalate <penpolpen@gmail.com>
2022-03-02 22:12:55 +0300

    added broken particle sound

--- Assets/Scripts/Doors/BrokenParticles.cs
+++ Assets/Scripts/Doors/BrokenParticles.cs
@@ -2,14 +2,21 @@
 
 public class BrokenParticles : MonoBehaviour
 {
+    [SerializeField] float startDelay = 5f;
+    [SerializeField] float repeatDelay = 5f;
+    AudioSource _audio;
+    [SerializeField] AudioClip brokenSound;
     [SerializeField] ParticleSystem brokenParticle;
 
-    void Update()
-    {
-        Invoke("ParticleBoom", 3);
+    void Start() {
+        _audio = GetComponent<AudioSource>();
+        InvokeRepeating("ParticleBoom", startDelay, repeatDelay);
     }
 
     void ParticleBoom() {
-        if (!brokenParticle.isPlaying) brokenParticle.Play();
+        if (!brokenParticle.isPlaying) {
+            brokenParticle.Play();
+            _audio.PlayOneShot(brokenSound);
+        } 
     }
 }
