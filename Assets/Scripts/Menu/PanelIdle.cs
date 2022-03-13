ff7c2d15a945a9184d405278e407cdc1f6e03afd
Nechalate <penpolpen@gmail.com>
2022-03-14 00:30:05 +0300

    added a interactive menu

---
+++ Assets/Scripts/Menu/PanelIdle.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PanelIdle : MonoBehaviour
+{
+    [SerializeField] Animator _anim;
+    StartGame _start;
+    void Awake()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    void Start() {
+        _anim.Play("IdleMenu");
+    }
+}
---
+++ Assets/Scripts/Menu/StartGame.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    public void StartedGame() {
+        LoadGame();
+    }
+    public void Quit() {
+        Application.Quit();
+    }
+
+    void LoadGame() {
+        SceneManager.LoadScene("Intro");
+    }
+}
