8016fc0cfbecf91195ef89467cf8ab0f6ddbfc06
Nechalate <p1-18@STUD.LOCAL>
2022-03-15 14:20:57 +0300

    improved user interface, improved loading screen

--- Assets/Scripts/Menu/SceneLoader/SceneLoader.cs
+++ Assets/Scripts/Menu/SceneLoader/SceneLoader.cs
@@ -1,23 +1,44 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField] Text _loadingText;
+    Text _loadTxt;
+
     public int loadLevel;
     public GameObject loadingScreen;
 
-    void Start()
+    void Awake()
     {
-        
-    }
-
-    void Update()
-    {
-        
+        _loadTxt = _loadingText.GetComponent<Text>();
     }
 
     public void Load() {
         loadingScreen.SetActive(true);
-        SceneManager.LoadScene(1);
+        
+        StartCoroutine(LoadAsync());
+    }
+
+    IEnumerator LoadAsync() {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(loadLevel);
+
+        asyncLoad.allowSceneActivation = false;
+
+        while (!asyncLoad.isDone) {
+            if (!asyncLoad.allowSceneActivation) {
+                _loadTxt.text = "Press any key";
+                if (Input.anyKeyDown) asyncLoad.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+    }
+
+    void Fade() {
+        _loadTxt.CrossFadeAlpha(0.0f, 0.5f, false);
+        
+        _loadTxt.CrossFadeAlpha(1.0f, 0.05f, false);
     }
 }
