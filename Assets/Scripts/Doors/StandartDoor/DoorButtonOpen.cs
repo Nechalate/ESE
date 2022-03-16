25016551c8a4a06dadcf512d84d8f07e56e78f4f
Nechalate <p1-18@STUD.LOCAL>
2022-03-16 14:19:05 +0300

    added a full functionality door for any place

---
+++ Assets/Scripts/Doors/StandartDoor/DoorButtonOpen.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorButtonOpen : MonoBehaviour
+{
+    [SerializeField] GameObject _doorButton;
+    [SerializeField] Vector3 startPosition;
+    [SerializeField] Vector3 endPosition;
+    [SerializeField] float step;
+    public float doorSpeed = 1;
+    private float progress;
+
+    DoorSystemButton _button;
+
+    void Awake() {
+        _button = _doorButton.GetComponent<DoorSystemButton>();
+    }
+
+    void Start() {
+        transform.localPosition = startPosition;
+    }
+
+    void FixedUpdate() {
+        DoorOpenAnim();
+    }
+
+    void DoorOpenAnim() {
+        if (_button.doorOpened) {
+            if (transform.localPosition.y <= 2.8f) {
+                transform.localPosition = new Vector3(0,progress, 0) * doorSpeed;
+                progress += step;
+            }
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Doors/StandartDoor/DoorSystemButton.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class DoorSystemButton : MonoBehaviour
+{
+    [SerializeField] GameObject _controller;
+    InteractiveScript _interact;
+    public bool doorOpened;
+    
+    void Awake() {
+        _interact = _controller.GetComponent<InteractiveScript>();
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.tag == "Player") {
+            if (_interact._interact) doorOpened = true;
+        }
+    }
+}
