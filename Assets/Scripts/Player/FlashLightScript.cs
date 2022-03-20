e1e8449ca92ae9827347063b5a68a172f2893067
Nechalate <penpolpen@gmail.com>
2022-03-20 23:50:00 +0300

    improved flashlight, now flashlight on/off on button

---
+++ Assets/Scripts/Player/FlashLightScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class FlashLightScript : MonoBehaviour
+{
+    [SerializeField] Light _pointLight;
+    void Start()
+    {
+        _pointLight.enabled = false;
+    }
+
+    void Update()
+    {
+        FlashLightControl();
+    }
+
+    void FlashLightControl() {
+        if (Input.GetKeyDown(KeyCode.F)) {
+            if (!_pointLight.enabled) _pointLight.enabled = true;
+            else _pointLight.enabled = false;
+        } 
+    }
+}
