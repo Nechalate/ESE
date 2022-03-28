b94eaab4b670f41f812dff0536089eb8023a4189
Nechalate <penpolpen@gmail.com>
2022-03-28 20:33:29 +0300

    improved electrical room

--- Assets/Scripts/Items/Battery/BatteryOn.cs
+++ Assets/Scripts/Items/Battery/BatteryOn.cs
@@ -1,21 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BatteryOn : MonoBehaviour
 {
     [SerializeField] GameObject controller;
     [SerializeField] Material _batteryMat;
     [SerializeField] AudioClip _powerOnSound;
+    [SerializeField] GameObject _pointLight;
     LightIsOn _light;
     AudioSource _audio;
     MeshRenderer _mesh;
+    Light _lightRender;
     
 
     void Awake() {
         _light = controller.GetComponent<LightIsOn>();
         _mesh = GetComponent<MeshRenderer>();
         _audio = GetComponent<AudioSource>();
+        _lightRender = _pointLight.GetComponent<Light>();
     }
 
     private void OnTriggerEnter(Collider other) {
@@ -23,6 +24,7 @@ public class BatteryOn : MonoBehaviour
             if (_light.BatteryTake) {
                 _light.BatteryIsOn = true;
                 _mesh.material = _batteryMat;
+                _lightRender.color = Color.green;
                 if (!_audio.isPlaying) _audio.PlayOneShot(_powerOnSound);
                 Destroy(GameObject.Find("BrokTip"));
             }
