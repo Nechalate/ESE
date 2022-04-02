847cb486870abb9c6685b9c85648acb5a331eb76
Nechalate <penpolpen@gmail.com>
2022-04-02 22:16:04 +0300

    added a way to gate D, added a new ambient music

---
+++ Assets/Scripts/Triggers/AmbientTrigger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class AmbientTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject _ambient;
+
+    private void OnTriggerEnter(Collider other) {
+        _ambient.SetActive(true);
+    }
+}
